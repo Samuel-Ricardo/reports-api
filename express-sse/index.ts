f12a2a928f12413f9374c120880d97346f7a919f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 09:04:53 -0300

    [:sparkles:] | send events

--- express-sse/index.ts
+++ express-sse/index.ts
@@ -9,4 +9,16 @@ app.get("/events", (req, res: Response) => {
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
   res.flushHeaders();
+
+  // send events continuously
+  const sendEvent = () => {
+    const data = {
+      message: "SSE Event",
+      timestamp: Date.now(),
+    };
+
+    res.write(`id: ${Math.random()} \n`);
+    res.write(`event: message \n`);
+    res.write(`data: ${JSON.stringify(data)} \n\n`);
+  };
 });
