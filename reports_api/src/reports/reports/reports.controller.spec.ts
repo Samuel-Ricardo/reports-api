ed5dca2d55cb2d84fb908d95ddc9a7df0f7e5760
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 16:53:44 -0300

    [:sparkles:] | create: route - GET > view (controller/reports)

--- reports_api/src/reports/reports/reports.controller.spec.ts
+++ reports_api/src/reports/reports/reports.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsController } from './reports.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { ReportsService } from './reports.service';
 
 describe('ReportsController', () => {
   let controller: ReportsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       controllers: [ReportsController],
+      providers: [ReportsService],
     }).compile();
 
     controller = module.get<ReportsController>(ReportsController);
--- reports_api/src/reports/reports/reports.controller.ts
+++ reports_api/src/reports/reports/reports.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private service: ReportsService) {}
+
+  @Get('view')
+  @Render('reports')
+  async view() {
+    const reports = await this.service.all();
+    return { reports };
+  }
+}
