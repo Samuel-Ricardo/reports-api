2d59cd4ec53ae146359b20ecaa0e76623269510d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 16:34:55 -0300

    [:sparkles:] | create: job - reports

--- reports_api/src/reports/reports.module.ts
+++ reports_api/src/reports/reports.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports/reports.service';
+import { BullModule } from '@nestjs/bull';
+import { ReportsJobService } from './reports-job/reports-job.service';
 
 @Module({
-  providers: [ReportsService],
+  imports: [
+    BullModule.registerQueue({
+      name: 'reports',
+    }),
+  ],
+  providers: [ReportsService, ReportsJobService],
 })
 export class ReportsModule {}
