434b2229d176f397a3cbbe289d24fbbf9bb766f5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 13:43:25 -0300

    [:sparkles:] | create: service - reports-job (reports)

--- reports_api/src/app.module.ts
+++ reports_api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ReportsJobService } from './reports/reports-job/reports-job.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ReportsJobService],
 })
 export class AppModule {}
