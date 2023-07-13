fe9ad3f47346bf2519e6253a45e28f874498e4cd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 16:46:15 -0300

    [:sparkles:] | create: controller -  reports

--- reports_api/src/reports/reports.module.ts
+++ reports_api/src/reports/reports.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports/reports.service';
 import { BullModule } from '@nestjs/bull';
 import { ReportsJobService } from './reports-job/reports-job.service';
+import { ReportsController } from './reports/reports.controller';
 
 @Module({
   imports: [
@@ -10,5 +11,6 @@ import { ReportsJobService } from './reports-job/reports-job.service';
     }),
   ],
   providers: [ReportsService, ReportsJobService],
+  controllers: [ReportsController],
 })
 export class ReportsModule {}
