3e757ca31146fcc2a58ba6c5dad27125c1f005aa
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 16:09:03 -0300

    [:sparkles:] | create: fun - request (service/reports)

--- reports_api/src/reports/reports/reports.service.ts
+++ reports_api/src/reports/reports/reports.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma/prisma.service';
+import { Status } from '@prisma/client';
 
 @Injectable()
 export class ReportsService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   all() {
-    return this.prismaService.report.findMany({
+    return this.prisma.report.findMany({
       orderBy: { created_at: 'desc' },
     });
   }
 
   findOne(id: string) {
-    return this.prismaService.report.findUnique({ where: { id } });
+    return this.prisma.report.findUnique({ where: { id } });
+  }
+
+  async request(filename?: string) {
+    const report = await this.prisma.report.create({
+      data: {
+        filename,
+        status: Status.PENDING,
+      },
+    });
+    return report;
   }
 }
