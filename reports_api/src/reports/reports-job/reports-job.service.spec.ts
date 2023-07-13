d3d89a208daaa61ae2c3ecd75b34b93b0e668bfc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 16:43:31 -0300

    [:sparkles:] | create: job  - produce (reports)

--- reports_api/src/reports/reports-job/reports-job.service.spec.ts
+++ reports_api/src/reports/reports-job/reports-job.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsJobService } from './reports-job.service';
+import { ReportsService } from '../reports/reports.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 describe('ReportsJobService', () => {
   let service: ReportsJobService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReportsJobService],
+      imports: [PrismaModule],
+      providers: [ReportsJobService, ReportsService],
     }).compile();
 
     service = module.get<ReportsJobService>(ReportsJobService);
--- reports_api/src/reports/reports-job/reports-job.service.ts
+++ reports_api/src/reports/reports-job/reports-job.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Process, Processor } from '@nestjs/bull';
+import { ReportsService } from '../reports/reports.service';
+import { Job } from 'bull';
 
-@Injectable()
-export class ReportsJobService {}
+@Processor('reports')
+export class ReportsJobService {
+  constructor(private reportsService: ReportsService) {}
+
+  @Process()
+  async produce(job: Job<{ reportID: string }>) {
+    await this.reportsService.produce(job.data.reportID);
+    return {};
+  }
+}
