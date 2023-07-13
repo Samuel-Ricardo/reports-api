e4b0fdeaad78c8ce101f704657ba9608c12d49e9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 10:48:31 -0300

    [:gear:] | setup: swagger (docs)

--- reports_api/src/main.ts
+++ reports_api/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('Reports API')
+    .setDescription('The Reports API implements Server Sent Events')
+    .setVersion('1.0')
+    .addTag('reports')
+    .setContact(
+      'Samuel',
+      'https://www.linkedin.com/in/samuel-ricardo/',
+      'samuelricardoofficial@gmail.com',
+    )
+    .setLicense(
+      'MIT',
+      'https://github.com/Samuel-Ricardo/reports-api/blob/main/LICENSE',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
