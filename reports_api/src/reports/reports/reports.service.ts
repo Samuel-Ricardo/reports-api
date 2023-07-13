cac5792564688ed51bb9f4aae47641093d4dd2ec
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-13 15:56:53 -0300

    [:sparkles:] | create: fun - all (service/reports)

--- reports_api/src/reports/reports/reports.service.ts
+++ reports_api/src/reports/reports/reports.service.ts
@@ -4,4 +4,10 @@ import { PrismaService } from '../../prisma/prisma/prisma.service';
 @Injectable()
 export class ReportsService {
   constructor(private prismaService: PrismaService) {}
+
+  all() {
+    return this.prismaService.report.findMany({
+      orderBy: { created_at: 'desc' },
+    });
+  }
 }
